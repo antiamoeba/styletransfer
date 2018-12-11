4b3097919d138c896ef37ce5d41f3fd9494061a4
mhwangc <mhwangc@berkeley.edu>
2018-12-11 13:25:50 -0800

    add pca patch matching

---
+++ patch_matching_pca.py
@@ -0,0 +1,86 @@
+import sklearn
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction import image
+import numpy as np
+from sklearn.decomposition import PCA
+import pdb
+
+class PatchMatcher:
+
+    # TODO: Make it so we don't actually have to save all the patches, like use indices instead?
+    """
+    S is the style image and patch_size is the length and width of the patch
+    """
+    def __init__(self, S, patch_size):
+        self.S = S
+        self.patch_size = patch_size
+        self.yw, self.xw = np.meshgrid(np.arange(self.patch_size), np.arange(self.patch_size), indexing="ij")
+        self.yw = self.yw.flatten()
+        self.xw = self.xw.flatten()
+        self.S_patches = []
+        self.matcher = None
+        self.pca = None
+        self.construct_matcher()
+
+    # TODO: Do we need to change the default heuristic of how to
+    # determine how patches are close?
+    """
+    Creates [patch images] and NearestNeighbors object for all patches in S
+    """
+    def construct_matcher(self):
+        patches = image.extract_patches_2d(self.S, (self.patch_size, self.patch_size))
+        self.S_patches = patches
+        flattened = np.reshape(patches, (patches.shape[0], -1))
+        normalized = flattened - flattened.mean(axis=1, keepdims=True)
+        pca = PCA(n_components = .95, svd_solver="full")
+        fitted = pca.fit_transform(normalized)
+        self.pca = pca
+        self.matcher = NearestNeighbors(n_neighbors=1, algorithm="auto").fit(fitted)
+
+    """
+    Input: img, sample_gap is subsampling gap
+
+    Output: [(y, x)] of upper-left corners of image neighborhoods
+
+    currently uses np.linspace (patches will overlap the normal
+    amount or even more)
+    """
+    def get_neighborhoods(self, img, sample_gap):
+        ys = np.linspace(0, img.shape[0] - self.patch_size, num=(img.shape[0]+sample_gap-1)//sample_gap, endpoint=True)
+        xs = np.linspace(0, img.shape[1] - self.patch_size, num=(img.shape[1]+sample_gap-1)//sample_gap, endpoint=True)
+        ys, xs = np.meshgrid(ys, xs)
+        ys, xs = ys.astype(int).flatten(), xs.astype(int).flatten()
+        return ys, xs
+    
+    # TODO: Add option to use other neighborhoods
+    """
+    Input: Guess image X, subsampling gap
+    subsampling gap should be <= patch size
+
+    Output: Corresponding patches for each sample from X to S
+    [[y1, y2, y3], [x1, x2, x3]] and [patch images],
+    (y1, x1) is the upper left corner
+    """
+    def find_nearest_neighbors(self, X, sample_gap):
+        neighb_ys, neighb_xs = self.get_neighborhoods(X, sample_gap)
+        X_patches = X[neighb_ys[:, None] + self.yw, neighb_xs[:, None] + self.xw, :]
+        X_patches = np.array(X_patches).reshape(len(X_patches), -1)
+
+        X_patches = X_patches - X_patches.mean(axis=1, keepdims=True)
+        transformed_X = pca.transform(X_patches)
+        distances, indices = self.matcher.kneighbors(transformed_X)
+
+        matches = self.S_patches[indices].reshape(X_patches.shape[0], self.patch_size, self.patch_size, 3)
+        neighborhoods = np.array([neighb_ys, neighb_xs])
+        return neighborhoods, matches
+
+if __name__ == "__main__":
+    import skimage.io as skio
+    import robust
+    import cv2
+    img = cv2.imread("images/style/starry_tiny.jpg")
+    img2 = cv2.imread("images/style/starry_tiny.jpg")
+    matcher = PatchMatcher(img, 35)
+    coords, images = matcher.find_nearest_neighbors(img, 30)
+    test = robust.less_robust_agg(coords, images, img, 35)
+    cv2.imwrite("patch_match_test.png", test)
